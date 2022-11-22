76d642617335dec08cf0efb8bb1c39f7a491e877
Akash Magnadia <akash.magnadia@gmail.com>
2022-11-22 14:11:26 -0600

    Loading another file by default
    Co-authored-by: Nikith Rachakonda <nikith97@gmail.com>
    Co-authored-by: Sravan Pattabhi <sravan.pattabhi4689@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const sampleFileURIName = 'y2.output'; // file to load by default
+const sampleFileURIName = 'y22.output'; // file to load by default
 const sampleFileURI = "../finite_automaton/sample_data_file/" + sampleFileURIName;
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
